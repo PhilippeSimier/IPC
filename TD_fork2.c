bbd528ee38d7bf5921d2310aaa0f749e3d58f5fe
PhilippeSimier <philaure@wanadoo.fr>
2016-09-25 10:45:30 +0200

    ajout TD fork exo 2

---
+++ TD_fork2.c
@@ -0,0 +1,39 @@
+/******************************************************************
+*    2) Dessinez l'arbre des processus généré			  *
+*       par le programme suivant:				  *
+*    Compilation : gcc TD_fork2.c -o TD_fork2			  *
+*******************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char *argv[] )
+{
+	int pid1,pid2,pid3,pid4;
+	pid4 = getpid(); 
+	pid1 = fork();
+	if ( pid1 == 0 )
+	{       // Enfant P1
+		printf ( "p1 pid=%d ppid=%d \n",getpid(),getppid());
+	}
+	else
+	{       // Père
+		pid2 = fork();
+		if ( pid2 == 0 )
+		{       // Enfant P2
+			printf ( "p2 pid=%d ppid=%d \n",getpid(),getppid());
+		}
+		else
+		{       // Père
+			pid3 = fork();
+			if ( pid3 == 0 )
+			{       // Enfant P3
+				printf ( "p3 pid=%d ppid=%d \n",getpid(),getppid());
+			}
+			else
+			{       // Père P4
+				printf ( "p4 pid=%d\n",pid4);
+			}
+		}
+	}
+	return EXIT_SUCCESS;
+}
