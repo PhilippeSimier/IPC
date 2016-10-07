0aba587dc8e288cbd869b334b47c7d265f458aaf
PhilippeSimier <philaure@wanadoo.fr>
2016-10-07 11:33:49 +0200

    ajout fin du td1 threads

---
+++ 5_THREADS/TD_thread1_suite.c
@@ -0,0 +1,156 @@
+/***********************************************************************************************
+*
+*     Corrigé du td n°5  élève question 3 et 4
+*
+*     Q3 Modifiez le comportement des threads afin que chacun mette à jour les 3 champs
+*     de la structure.
+*
+*     Q4 Modifiez votre programme pour que la validité des données soit assurée (mutex)
+*
+*     Compilation: gcc TD_thread1_suite.c -o TD_thread1_suite -l pthread -std=c99
+*
+************************************************************************************************/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+
+
+
+typedef struct {
+    int v1;
+    int v2;
+    int v3;
+}laStruct;
+
+laStruct        global;
+pthread_mutex_t verrou = PTHREAD_MUTEX_INITIALIZER; /** Création du mutex et initialisation**/
+
+void *tache1(void *p_data)
+{
+
+    laStruct val = *((laStruct *) p_data);
+    int tid;
+
+    pthread_mutex_lock(&verrou); /** On verrouille le mutex **/
+    global = val;
+    pthread_mutex_unlock (&verrou); /** On déverrouille le mutex **/
+
+    tid = syscall(SYS_gettid);
+    printf("je suis T1 TID = %d v1 = %d,\n", tid, global.v1);
+    printf("dans T1 l'argument vaut : %d\n", val.v1);
+    sleep(3);
+
+    pthread_exit((void *)"Job terminé pour T1 \n");
+}
+
+void *tache2(void *p_data)
+{
+
+
+    laStruct val = *((laStruct *) p_data);
+    int tid;
+    int j = 0;
+
+    for (int i=0 ; i< 10000; i++){     /** boucle pour retarder la tache2 **/
+        j *= 2;
+    }
+
+
+
+    pthread_mutex_lock(&verrou); /** On verrouille le mutex **/
+    global = val;
+    pthread_mutex_unlock (&verrou); /** On déverrouille le mutex **/
+
+    tid = syscall(SYS_gettid);
+
+    printf("je suis T2 TID = %d v2 = %d,\n", tid, global.v2);
+    printf("dans T2 l'argument vaut : %d\n", val.v2);
+    sleep(1);
+    pthread_exit((void *)"job terminé pour T2 \n");
+}
+
+void *tache3(void *p_data)
+{
+
+
+    laStruct val = *((laStruct *) p_data);
+    int tid;
+
+    pthread_mutex_lock(&verrou); /** On verrouille le mutex **/
+    global = val;
+    pthread_mutex_unlock (&verrou); /** On déverrouille le mutex **/
+
+    tid = syscall(SYS_gettid);
+    printf("je suis T3 TID = %d v3 = %d,\n", tid, global.v3);
+    printf("dans T3 l'argument vaut : %d\n", val.v3);
+    sleep(2);
+
+    pthread_exit((void *)"Job terminé pour T3 \n");
+}
+
+int main()
+{
+    int i,res;
+    pthread_t T[3];
+
+
+    void *thread_result;
+
+
+
+    laStruct a = {11,12,13};
+    laStruct b = {21,22,23};
+    laStruct c = {31,32,33};
+
+    if (pthread_mutex_init(&verrou, NULL) != 0)
+    {
+        printf("Mutex init failed\n");
+        return 1;
+    }
+
+    /** Création des trois threads  **/
+
+
+    res = pthread_create(&T[0], NULL, tache1, (void *)&a);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_create(&T[1], NULL, tache2, (void *)&b);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    res = pthread_create(&T[2], NULL, tache3, (void *)&c);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    /** attente de la fin des trois threads **/
+
+    printf("Attente de la fin des thread...\n");
+
+    for ( i = 0; i < 3; i++){
+        res = pthread_join(T[i], &thread_result);
+        if (res != 0) {
+            perror("Thread join a échoué");
+            exit(EXIT_FAILURE);
+        }
+        printf("OK, valeur de retour du thread join : %s\n", (char *)thread_result);
+    }
+
+    /** affichage de la variable globale **/
+
+    printf(" V1 = %d\n V2 = %d\n V3 = %d\n", global.v1, global.v2, global.v3);
+
+    pthread_mutex_destroy(&verrou);
+
+    exit(EXIT_SUCCESS);
+}
