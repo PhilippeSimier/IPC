ed7aa23259ff5f969ec7a7a7a2c244d04d421a4e
philippeSimier <philaure@wanadoo.fr>
2018-09-21 08:51:21 +0200

    add sujet & update#

--- 1_FORK/TD_fork1.c
+++ 1_FORK/TD_fork1.c
@@ -1,10 +1,11 @@
 /****************************************************
 *  1) Donnez l'affichage du programme suivant:      *
 *     Combien d'étoiles vont'elles s'afficher ?	    *
-*  Compilation: gcc TD_fork1.c -o TD_fork1          *
+*     Compilation: gcc TD_fork1.c -o TD_fork1       *
 *****************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>   // Biblio pour programmation systeme (fork wait ...)
 
 int main ( int argc, char *argv[] )
 {
